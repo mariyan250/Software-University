e951c00a6a32cef3fdc74dec9161aea83343128d
mariyan250 <mariyan250@gmail.com>
2019-11-25 19:02:52 +0200

    release

---
+++ JS Fundamentals/10. Exam Preparation/02. Final Exam/followers.js
@@ -0,0 +1,81 @@
+function solve(input) {
+    let obj = {}
+
+    for (const line of input) {
+        let command = line.split(": ")[0];
+
+        switch (command) {
+            case 'New follower':
+                let followerUsername = line.split(": ")[1];
+
+                if (!obj.hasOwnProperty(followerUsername)) {
+                    obj[followerUsername] = {
+                        likes: 0,
+                        comments: 0
+                    }
+                }
+
+                break;
+
+            case 'Like':
+                let username = line.split(": ")[1];
+                let likesCount = Number(line.split(": ")[2]);
+
+                if (obj.hasOwnProperty(username)) {
+                    obj[username].likes += likesCount;
+                } else {
+                    obj[username] = {
+                        likes: likesCount,
+                        comments: 0
+                    }
+                }
+
+                break;
+
+            case 'Comment':
+                let user = line.split(": ")[1];
+
+                if (obj.hasOwnProperty(user)) {
+                    obj[user].comments += 1;
+                } else {
+                    obj[user] = {
+                        likes: 0,
+                        comments: 1
+                    }
+                }
+
+                break;
+
+            case 'Blocked':
+                let blockedUsername = line.split(": ")[1];
+
+                if (obj.hasOwnProperty(blockedUsername)) {
+                    delete obj[blockedUsername];
+                } else {
+                    console.log(`${blockedUsername} doesn't exist.`);
+                }
+
+                break;
+        }
+    }
+
+    console.log(`${Object.keys(obj).length} followers`);
+
+    Object.keys(obj).sort((a, b) => {
+            return obj[b].likes - obj[a].likes || a.localeCompare(b);
+        })
+        .forEach(follower => {
+            console.log(`${follower}: ${obj[follower].likes + obj[follower].comments}`);
+        })
+}
+
+solve([
+    'Like: A: 3',
+    'Comment: A',
+    'New follower: B',
+    'Blocked: A',
+    'Comment: B',
+    'Like: C: 5',
+    'Like: D: 5',
+    'Log out'
+]);
