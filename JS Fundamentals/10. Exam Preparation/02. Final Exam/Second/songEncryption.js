ae3584db35796b0e23f984cc158fa150b1938b60
Mariyan Velichkov <41576873+mariyan250@users.noreply.github.com>
2019-11-30 14:10:33 +0200

    release

--- JS Fundamentals/10. Exam Preparation/02. Final Exam/Second/songEncryption.js
+++ JS Fundamentals/10. Exam Preparation/02. Final Exam/Second/songEncryption.js
@@ -1,41 +1,43 @@
 function solve(input) {
     for (const line of input) {
-        if (line == 'end') {
+        if (line == "end") {
             break;
         }
 
-        let regex = /^([A-Z'][a-z']+[\s]*[a-z']+):([A-Z+\s*A-Z]+)$/g;
+        let regex = /^([A-Z'][a-z']+[\s]*[a-z']+):([A-Z]+[\s*[A-Z]+)$/g;
 
         if (line.match(regex)) {
             let arr = regex.exec(line);
             let [artist, song] = arr.slice(1);
 
-            let key = artist.length;
+            let artistLength = artist.length;
 
             let result1 = artist.split('').map(symbol => {
-                let result = symbol.toLowerCase().charCodeAt(0) + key;
-                if (result >= 65 && result <= 90 || result >= 97 && result <= 122) {
+                let result = symbol.charCodeAt(0) + artistLength;
+                if (result >= 60 && result <= 90 || result >= 97 && result <= 122) {
                     return String.fromCharCode(result);
-                } else if (symbol == ' ' || symbol == "'") {
+                } else if (symbol == " " || symbol == "'") {
                     return symbol;
+                } else if (result > 90 && result < 97) {
+                    let left = result - 90;
+                    let toAdd = String.fromCharCode(97 + left - 1);
+                    return toAdd;
                 } else if (result > 122) {
                     let left = result - 122;
                     let toAdd = String.fromCharCode(97 + left - 1);
                     return toAdd;
                 }
-            });
 
-            result1[0] = result1[0].toUpperCase();
-            result1 = result1.join('');
+            }).join('');
 
             let result2 = song.split('').map(symbol => {
-                let result = symbol.toLowerCase().charCodeAt(0) + key;
-                if (result >= 97 && result <= 122) {
+                let result = symbol.charCodeAt(0) + artistLength;
+                if (result >= 60 && result <= 90) {
                     return String.fromCharCode(result);
-                } else if (symbol == ' ') {
+                } else if (symbol == " ") {
                     return symbol;
-                } else if (result > 122) {
-                    let left = result - 122;
+                } else if (result > 90) {
+                    let left = result - 90;
                     let toAdd = String.fromCharCode(97 + left - 1);
                     return toAdd;
                 }
@@ -43,7 +45,7 @@ function solve(input) {
 
             console.log(`Successful encryption: ${result1}@${result2}`);
         } else {
-            console.log('Invalid input!');
+            console.log("Invalid input!");
         }
     }
 }
