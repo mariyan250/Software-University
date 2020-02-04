9b2bd936077325c67731fc8af22e6b7b47f98321
mariyan250 <mariyan250@gmail.com>
2020-02-04 16:32:34 +0200

    release

--- JS Advanced/JS Advanced/07. Classes/03. unity.js
+++ JS Advanced/JS Advanced/07. Classes/03. unity.js
@@ -1,1 +1,24 @@
-// Todo..
+class Rat {
+    constructor(name) {
+        this.name = name;
+        this.unitedRats = [];
+    }
+
+    unite(otherRat) {
+        if (otherRat.hasOwnProperty('name') && otherRat.hasOwnProperty('unitedRats')) {
+            this.unitedRats.push(otherRat);
+        }
+    }
+
+    getRats() {
+        return this.unitedRats;
+    }
+
+    toString() {
+        if (this.unitedRats.length > 0) {
+            return `${this.name}\n${this.unitedRats.map(rat => `##${rat}`).join("\n")}`;
+        } else {
+            return this.name;
+        }
+    }
+}
